007ccb28797e86433e57e9713030f1dd41d29ea5
James Flores <jamesflores@HA21457FALK.local>
2017-09-25 20:25:09 -0500

    Added success and invalid username templates.  Having difficulty posting error message next to text box.

--- signup_main.py
+++ signup_main.py
@@ -9,5 +9,14 @@ app.config['DEBUG'] = True
 def index():
     return render_template('form_fields.html')
 
+@app.route("/", methods=['POST'])
+def user_name_valid():
+    user_name = request.form['username']
+
+    if len(user_name)<3 or len(user_name)>20:
+        return render_template('invalid_username.html')
+
+    return render_template('success.html')
+
 
 app.run()
