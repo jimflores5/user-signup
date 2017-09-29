e769f669901e8083ba470a2d9c3b315452ab8c5e
James Flores <jamesflores@ha21457falk.claytonschools.stl>
2017-09-28 23:37:26 -0500

    Figured out error message displayed at wrong time.  Began code for  verification of password.

--- signup_main.py
+++ signup_main.py
@@ -12,9 +12,33 @@ def index():
 @app.route("/", methods=['POST'])
 def user_name_valid():
     user_name = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify']
 
-    if len(user_name)<3 or len(user_name)>20:
-        return render_template('invalid_username.html')
+    if user_name == '':
+        name_error = 'Please enter a user name.'
+        return render_template('form_fields.html',name=user_name,name_error=name_error)
+    elif len(user_name)<3 or len(user_name)>20:
+        name_error = 'User name must be between 3 & 20 characters.'
+        user_name = ''
+        return render_template('form_fields.html',name=user_name,name_error=name_error)
+    elif " " in user_name:
+        name_error = 'Username cannot contain spaces.'
+        user_name = ''
+        return render_template('form_fields.html',name=user_name,name_error=name_error)
+
+    if password == '':
+        pass_error = 'Please enter a password.'
+        return render_template('form_fields.html',password=password,pass_error=pass_error)
+    elif len(password)<3 or len(password)>10:
+        pass_error = 'Password must be 3 - 10 characters long.'
+        password = ''
+        return render_template('form_fields.html',password=password,pass_error=pass_error)
+    elif " " in password:
+        pass_error = 'Password cannot contain spaces.'
+        password = ''
+        return render_template('form_fields.html',password=password,pass_error=pass_error)
+    
 
     return render_template('success.html')
 
