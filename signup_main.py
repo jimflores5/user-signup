835b4e8895cc920c5f195c3bad1f6aa434ee35a8
Jim Flores <jimflores5@gmail.com>
2017-10-01 16:19:33 -0500

    Finally got all 4 fields set up and verified.

--- signup_main.py
+++ signup_main.py
@@ -14,18 +14,19 @@ def user_name_valid():
     user_name = request.form['username']
     password = request.form['password']
     verify_password = request.form['verify']
+    email = request.form['email']
 
     name_error = ''
     pass_error = ''
+    verify_error =''
+    mail_error = ''
 
     if user_name == '':
         name_error = 'Please enter a user name.'
     elif len(user_name)<3 or len(user_name)>20:
         name_error = 'User name must be 3 - 20 characters long.'
-        user_name = ''
     elif " " in user_name:
         name_error = 'Username cannot contain spaces.'
-        user_name = ''
 
     if password == '':
         pass_error = 'Please enter a password.'
@@ -37,13 +38,26 @@ def user_name_valid():
         pass_error = 'Password cannot contain spaces.'
         password = ''
         verify_password = ''
-    elif verify_password != password:
-        pass_error = 'Passwords do not match.'
+    elif verify_password =='':
+        verify_error = 'Please verify your password.'
         verify_password = ''
+    elif verify_password != password:
+        verify_error = 'Passwords do not match.'
+        password = ''
+        verify_password = ''
+
+    if len(email)<3 or len(email)>20:
+        mail_error = 'E-mail address must be 3 - 20 characters long.'
+    elif " " in email:
+        mail_error = 'E-mail address cannot contain spaces.'
+    elif email.count('@') != 1:
+        mail_error = 'E-mail address must contain one "@" symbol.'
+    elif email.count('.') != 1:
+        mail_error = 'E-mail address must contain one "." symbol.'
         
-    if name_error == '' and pass_error == '':
-        return render_template('success.html')
+    if name_error == '' and pass_error == '' and verify_error == '' and mail_error=='':
+        return render_template('success.html',name=user_name)
     else:
-        return render_template('form_fields.html',name=user_name, password=password,verify_password=verify_password,name_error=name_error,pass_error=pass_error)
+        return render_template('form_fields.html',name=user_name, password=password,verify_password=verify_password,email=email,name_error=name_error,pass_error=pass_error,verify_error=verify_error,email_error=mail_error)
 
 app.run()
